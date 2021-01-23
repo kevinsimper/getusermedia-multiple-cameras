a42baad25b36b60b77e0479f4bf73d6ac1a63b0d
Kevin Simper <kevin.simper@gmail.com>
2021-01-23 01:36:04 +0100

    Run when pressing button to help on mobile devices with permissions

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,54 @@
-navigator.mediaDevices.enumerateDevices().then((devices) => {
-  const videos = devices.filter((device) => {
-    return device.kind === "videoinput";
-  });
-  document.querySelector("#cameras").innerHTML = `${videos
-    .map((video, index) => {
-      return `<div>
-        <video style="width: 100%;" id="video${index}"></video>
-      </div>`;
-    })
-    .join("")}`;
-  videos.forEach((video, index) => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { deviceId: video.deviceId } })
-      .then(function (mediaStream) {
-        let videoElement: HTMLVideoElement = document.querySelector(
-          "#video" + index
-        );
-        videoElement.setAttribute("playsinline", "true");
-        videoElement.srcObject = mediaStream;
-        videoElement.onloadedmetadata = function (e) {
-          videoElement.play();
-        };
-      })
-      .catch(function (err) {
-        console.log(err.name + ": " + err.message);
-        alert(err);
-      });
-  });
+const startButton = document.querySelector("#start");
 
-  document.querySelector("#devices").innerHTML = JSON.stringify(
-    devices,
-    null,
-    2
-  );
+startButton.addEventListener("click", () => {
+  startCameras();
 });
 
-setTimeout(() => {
-  var supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+function startCameras() {
+  navigator.mediaDevices.enumerateDevices().then((devices) => {
+    const videos = devices.filter((device) => {
+      return device.kind === "videoinput";
+    });
+    document.querySelector("#cameras").innerHTML = `${videos
+      .map((video, index) => {
+        return `<div>
+          <video style="width: 100%;" id="video${index}"></video>
+        </div>`;
+      })
+      .join("")}`;
+    videos.forEach((video, index) => {
+      navigator.mediaDevices
+        .getUserMedia({ video: { deviceId: video.deviceId } })
+        .then(function (mediaStream) {
+          let videoElement: HTMLVideoElement = document.querySelector(
+            "#video" + index
+          );
+          videoElement.setAttribute("playsinline", "true");
+          videoElement.srcObject = mediaStream;
+          videoElement.onloadedmetadata = function (e) {
+            videoElement.play();
+          };
+        })
+        .catch(function (err) {
+          console.log(err.name + ": " + err.message);
+          alert(err);
+        });
+    });
 
-  document.querySelector("#constraints").innerHTML = JSON.stringify(
-    supportedConstraints,
-    null,
-    2
-  );
-}, 2000);
+    document.querySelector("#devices").innerHTML = JSON.stringify(
+      devices,
+      null,
+      2
+    );
+  });
+
+  setTimeout(() => {
+    var supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+
+    document.querySelector("#constraints").innerHTML = JSON.stringify(
+      supportedConstraints,
+      null,
+      2
+    );
+  }, 2000);
+}
