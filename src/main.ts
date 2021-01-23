0512e1096f782ceead73fe347e974bcac0b8618c
Kevin Simper <kevin.simper@gmail.com>
2021-01-23 01:57:38 +0100

    ios only shows deviceid in enumerateDevices after getusermedia has been accepted

--- src/main.ts
+++ src/main.ts
@@ -4,42 +4,43 @@ startButton.addEventListener("click", () => {
   startCameras();
 });
 
-function startCameras() {
-  navigator.mediaDevices.enumerateDevices().then((devices) => {
-    const videos = devices.filter((device) => {
-      return device.kind === "videoinput";
-    });
-    document.querySelector("#cameras").innerHTML = `${videos
-      .map((video, index) => {
-        return `<div>
+async function startCameras() {
+  await navigator.mediaDevices.getUserMedia({ video: true });
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videos = devices.filter((device) => {
+    return device.kind === "videoinput";
+  });
+
+  document.querySelector("#cameras").innerHTML = `${videos
+    .map((video, index) => {
+      return `<div>
           <video style="width: 100%;" id="video${index}"></video>
         </div>`;
-      })
-      .join("")}`;
-    videos.forEach((video, index) => {
-      navigator.mediaDevices
-        .getUserMedia({ video: { deviceId: video.deviceId } })
-        .then(function (mediaStream) {
-          let videoElement: HTMLVideoElement = document.querySelector(
-            "#video" + index
-          );
-          videoElement.setAttribute("playsinline", "true");
-          videoElement.srcObject = mediaStream;
-          videoElement.onloadedmetadata = function (e) {
-            videoElement.play();
-          };
-        })
-        .catch(function (err) {
-          console.log(err.name + ": " + err.message);
-          alert(err);
-        });
-    });
+    })
+    .join("")}`;
 
-    document.querySelector("#devices").innerHTML = JSON.stringify(
-      devices,
-      null,
-      2
-    );
+  videos.forEach((video, index) => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { deviceId: video.deviceId } })
+      .then(function (mediaStream) {
+        let videoElement: HTMLVideoElement = document.querySelector(
+          "#video" + index
+        );
+        videoElement.setAttribute("playsinline", "true");
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = function (e) {
+          videoElement.play();
+        };
+        document.querySelector("#devices").innerHTML = JSON.stringify(
+          devices,
+          null,
+          2
+        );
+      })
+      .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+        alert(err);
+      });
   });
 
   setTimeout(() => {
